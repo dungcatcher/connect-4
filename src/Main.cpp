78d9660d468ec6c33cd420728b192d1fd859b4ba
dungcatcher <ewan.odenthal1@gmail.com>
2022-02-04 22:53:27 +1100

    First commit

---
+++ src/Main.cpp
@@ -0,0 +1,214 @@
+#include "Platform/Platform.hpp"
+#include <stdint.h>
+
+uint64_t redPieces = 0;
+uint64_t yellowPieces = 0;
+
+// const uint64_t bitColumns[] = { 0x40810204081, 0x81020408102, 0x102040810204, 0x204081020408, 0x408102040810, 0x810204081020, 0x1020408102040 };
+
+//  Board representation
+//  35 36 37 38 39 40 41
+//  28 29 30 31 32 33 34
+//  21 22 23 24 25 26 27
+//  14 15 16 17 18 19 20
+//  7  8  9  10 11 12 13
+//  0  1  2   3  4  5  6
+
+bool detectWin(bool turn);
+bool detectWin(bool turn)
+{
+	uint64_t bitboard;
+	if (turn)
+		bitboard = redPieces;
+	else
+		bitboard = yellowPieces;
+
+	// Check south east diagonal
+	uint64_t tempBoard = bitboard & (bitboard >> 6);
+	if (tempBoard & (tempBoard >> 2 * 6))
+		return true;
+	// Check vertical
+	tempBoard = bitboard & (bitboard >> 7);
+	if (tempBoard & (tempBoard >> 2 * 7))
+		return true;
+	// Check north east diagonal
+	tempBoard = bitboard & (bitboard >> 8);
+	if (tempBoard & (tempBoard >> 2 * 8))
+		return true;
+	// Check horizontal
+	tempBoard = bitboard & (bitboard >> 1);
+	if (tempBoard & (tempBoard >> 2 * 1))
+		return true;
+
+	return false;
+}
+
+void updateBoard(sf::RenderWindow* window, sf::Sprite* boardSprite, bool turn);
+void updateBoard(sf::RenderWindow* window, sf::Sprite* boardSprite, bool turn)
+{
+	window->clear();
+	window->draw(*boardSprite);
+
+	sf::CircleShape redPiece(60);
+	redPiece.setFillColor(sf::Color::Red);
+	redPiece.setOrigin(sf::Vector2f(60, 60));
+	sf::CircleShape yellowPiece(60);
+	yellowPiece.setFillColor(sf::Color::Yellow);
+	yellowPiece.setOrigin(sf::Vector2f(60, 60));
+
+	if (detectWin(turn))
+	{
+		std::cout << "win\n";
+	}
+
+	int testBit = 0;
+	uint64_t checkRedBits = redPieces;
+	uint64_t checkYellowBits = yellowPieces;
+
+	while (testBit < 42)
+	{
+		if (checkRedBits & 0x01)
+		{
+			const int xPos = testBit % 7;
+			const int yPos = 5 - testBit / 7;
+			redPiece.setPosition(sf::Vector2f(xPos * (window->getSize().x / 7) + window->getSize().x / 14, yPos * (window->getSize().y / 6) + window->getSize().y / 12));
+			window->draw(redPiece);
+		}
+		if (checkYellowBits & 0x01)
+		{
+			const int xPos = testBit % 7;
+			const int yPos = 5 - testBit / 7;
+			yellowPiece.setPosition(sf::Vector2f(xPos * (window->getSize().x / 7) + window->getSize().x / 14, yPos * (window->getSize().y / 6) + window->getSize().y / 12));
+			window->draw(yellowPiece);
+		}
+
+		testBit++;
+		checkRedBits >>= 1;
+		checkYellowBits >>= 1;
+	}
+	window->display();
+}
+
+int generateMove(int column);
+int generateMove(int column)
+{
+	// Returns the y value of the column clicked on
+	uint64_t vacantPositions = ~(redPieces | yellowPieces);
+	vacantPositions >>= column;
+	int row = -1; // If row is -1 there are no legal spaces
+
+	for (unsigned int i = 0; i < 6; i++)
+	{
+		if (vacantPositions & 0x01)
+		{
+			row = i;
+			break;
+		}
+		vacantPositions >>= 7;
+	}
+
+	return row;
+}
+
+uint64_t makeMove(int row, int col, uint64_t board);
+uint64_t makeMove(int row, int col, uint64_t board)
+{
+	board |= ((uint64_t)1 << (row * 7 + col));
+	return board;
+}
+
+long int perft(int currentPly, int maxPly, bool turn, uint64_t redBoard, uint64_t yellowBoard);
+long int perft(int currentPly, int maxPly, bool turn, uint64_t redBoard, uint64_t yellowBoard)
+{
+	long int nodes = 0;
+
+	const bool continueSearch = currentPly < maxPly;
+
+	for (int col = 0; col < 7; col++)
+	{
+		int row = generateMove(col);
+		if (row != -1)
+		{
+			if (continueSearch)
+			{
+				uint64_t newBoard = makeMove(row, col, (turn) ? redBoard : yellowBoard);
+				nodes += perft(currentPly + 1, maxPly, !turn, (!turn) ? newBoard : redBoard, (turn) ? newBoard : yellowBoard);
+			}
+			else
+			{
+				nodes++;
+			}
+		}
+	}
+	return nodes;
+}
+
+int main()
+{
+	util::Platform platform;
+
+#if defined(_DEBUG)
+	std::cout << "Hello World!" << std::endl;
+#endif
+
+	sf::RenderWindow window;
+	// in Windows at least, this must be called before creating the window
+	float screenScalingFactor = platform.getScreenScalingFactor(window.getSystemHandle());
+	// Use the screenScalingFactor
+	window.create(sf::VideoMode(700.0f * screenScalingFactor, 600.0f * screenScalingFactor), "Connect 4");
+	platform.setIcon(window.getSystemHandle());
+
+	sf::Texture boardTexture;
+	boardTexture.loadFromFile("./content/board.png");
+	sf::Sprite boardSprite;
+	boardSprite.setTexture(boardTexture);
+	boardSprite.setScale(sf::Vector2f(screenScalingFactor, screenScalingFactor));
+
+	bool turn = 1; // 1 = red, 0 = yellow
+	bool mouseDown = false;
+
+	std::cout << perft(1, 9, 1, redPieces, yellowPieces) << "\n";
+
+	window.setFramerateLimit(60);
+
+	sf::Event event;
+
+	while (window.isOpen())
+	{
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+			{
+				if (!mouseDown)
+				{
+					int column = sf::Mouse::getPosition(window).x / (window.getSize().x / 7);
+					int row = generateMove(column);
+					if (row != -1)
+					{
+						unsigned int bitPosition = row * 7 + column;
+						std::cout << bitPosition << "\n";
+						if (turn)
+						{
+							redPieces |= ((uint64_t)1 << bitPosition); // Edit red pieces bit
+							turn = 0;
+						}
+						else
+						{
+							yellowPieces |= ((uint64_t)1 << bitPosition); // Edit yellow pieces bit
+							turn = 1;
+						}
+					}
+				}
+				mouseDown = true;
+			}
+			else
+				mouseDown = false;
+		}
+
+		updateBoard(&window, &boardSprite, turn);
+	}
+
+	return 0;
+}
